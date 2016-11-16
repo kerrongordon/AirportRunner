3e40ad37c7a9bc8940708de07db6b979940c5ac3
kerrongordon <kgpsounds.com@gmail.com>
2016-11-15 21:22:23 -0400

    added fonts and fix menu button bug

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -1,8 +1,7 @@
 
 extends Node
 
-var index = 0
-
+# get all buttons on the main menu screen
 onready var startGame = get_node("main_menu_btn_start")
 onready var gameOption = get_node("main_menu_btn_option")
 onready var quitGame = get_node("main_menu_btn_quit")
@@ -11,16 +10,20 @@ func _ready():
 	set_fixed_process(true)
 	set_process_input(true)
 	#Set window title
-	OS.set_window_title("Just A Game Menu")
+	OS.set_window_title("Menu Menu")
 
 func _input(event):
-	if ( startGame.is_pressed() and !event.is_echo() ):
+	if event.type == 2: return
+	#button to start the game	
+	if ( startGame.is_pressed() and not event.is_echo() ):
 		print('start gamae')
-
-	if ( gameOption.is_pressed() and !event.is_echo() ):
+		
+	#buton to go to the option menu
+	if ( gameOption.is_pressed() and not event.is_echo() ):
 		get_parent().get_node("view").get_node("menu_anim").play("menu-to-option")
 		print('option')
-	
-	if ( quitGame.is_pressed() and !event.is_echo() ):
+		
+	#button to quit the game
+	if ( quitGame.is_pressed() and not event.is_echo() ):
 		OS.get_main_loop().quit()
 	
--- scripts/menu/menu_option.gd
+++ scripts/menu/menu_option.gd
@@ -1,14 +1,18 @@
 
 extends Node
 
+#get the back button 
 onready var goBackToMenu = get_node("option_menu_btn_back")
 
 func _ready():
 	set_process_input(true)
 	
 func _input(event):
+	if event.type == 2: return
+	
+	#button to take me back to the main menu
 	if ( goBackToMenu.is_pressed() and !event.is_echo() ):
 		get_parent().get_node("view/menu_anim").play("option-to-menu")
-		print("go back")
+
 
 
